7c0a3bae44b31803b30c447a3e72cfc67039d24d
Fornax <realfornax@gmail.com>
2023-08-11 18:46:13 -0700

    Random

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,31 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
-  *main
-  *Description: Code to check number nature
-  *
-  */
-
-
+ * main - Entry point of the program.
+ *
+ * This program generates a random number, assigns it to variable n,
+ * and determines whether the number is positive or negative.
+ *
+ * Return: Always 0 (success).
+ */
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        if (n > 0) 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
 	{
-                printf("%d is positive.\n", n);
-        } 
-	else if (n < 0) 
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
 	{
-                printf("%d is negative.\n", n);
-        } 
-	else 
+		printf("%d is negative\n", n);
+	}
+	else
 	{
-                printf("%d is zero.\n", n);
-        }
-        return (0);
+		printf("%d is zero\n", n);
+	}
+	return (0);
 }
